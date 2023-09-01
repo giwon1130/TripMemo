ec378d65bc68d278420c12a1f873939e834d9508
임기원 <imgiwon@imgiwon-ui-MacBookPro.local>
2023-09-01 14:43:28 +0900

    setting api 추가

--- src/main/java/com/ig/tripmemo/repository/entity/Settings.java
+++ src/main/java/com/ig/tripmemo/repository/entity/Settings.java
@@ -1,25 +1,34 @@
 package com.ig.tripmemo.repository.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@ApiModel(description = "All details about the Settings.")
 public class Settings {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The database generated setting ID")
     private Long id;
+
+    @ApiModelProperty(notes = "The type of the map")
     private String mapType;
+
+    @ApiModelProperty(notes = "The URL of the map")
     private String mapUrl;
-    private Geometry startPoint;
+
+    @Column(columnDefinition = "geometry(Point, 4326)")
+    @ApiModelProperty(notes = "The starting point of the map in geometry format")
+    private Point startingPoint;
+
+    @ApiModelProperty(notes = "The zoom level of the map")
     private Integer zoomLevel;
 
-    // getters and setters
 }
